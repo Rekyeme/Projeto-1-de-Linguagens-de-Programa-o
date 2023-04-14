f7f316e43876776360447b5a38333f5411837c9e
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-04-14 14:51:04 +0100

    Base project creation

---
+++ Puzzle_das_Lâmpadas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Puzzle_das_Lâmpadas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
